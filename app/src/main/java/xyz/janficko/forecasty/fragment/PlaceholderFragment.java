3969c88560596bf8e1bfc4350d360a34b673509a
Jan Ficko <jan.ficko@gmail.com>
2016-08-09 14:11:36 +0200

    Added listview

--- app/src/main/java/xyz/janficko/forecasty/fragment/PlaceholderFragment.java
+++ app/src/main/java/xyz/janficko/forecasty/fragment/PlaceholderFragment.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import xyz.janficko.forecasty.R;
 
@@ -18,6 +22,27 @@ public class PlaceholderFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> vreme = new ArrayList<String>();
+        vreme.add("Danes - Sončno - 25/17");
+        vreme.add("Jutri - Sončno - 25/17");
+        vreme.add("Sreda - Sončno - 25/17");
+        vreme.add("Četrtek - Sončno - 25/17");
+        vreme.add("Petek - Sončno - 25/17");
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+                // The current context (this fragment's parent activity)
+                getActivity(),
+                // ID of the list item layout
+                R.layout.list_item_forecast,
+                // ID of textview to populate
+                R.id.list_item_forecast_textview,
+                // Forecast data
+                vreme);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+
         return rootView;
     }
 }
